82ea2d08697d01c4e2b22c7b3bafd912dc037eae
diegonorman <diegonorman5@gmail.com>
2025-10-07 18:09:45 -0300

    Treinov10

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ function initGoogleAuth() {
 }
 
 function loginGoogle() {
-    const authUrl = `https://accounts.google.com/oauth/authorize?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${window.location.origin}&scope=https://www.googleapis.com/auth/drive.readonly&response_type=token`;
+    const authUrl = `https://accounts.google.com/oauth/authorize?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=https://diegonorman.github.io/treinor-norman/&scope=https://www.googleapis.com/auth/drive.readonly&response_type=token`;
     window.location.href = authUrl;
 }
 
